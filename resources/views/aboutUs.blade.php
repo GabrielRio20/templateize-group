943d17090e8a12113f2bf97ddd0ff8a1cccbf1fc
Mumtaza15 <ahmad.fatha1502@mail.ugm.ac.id>
2022-12-14 12:10:17 +0700

    aboutUsNew

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -15,7 +15,7 @@
                 padding: 0;
                 margin: 0;
                 box-sizing: border-box;
-                /* font-family: 'Dosis', sans-serif; */
+                font-family: 'Dosis', sans-serif;
                 outline: none;
             }
             .about{
